c771d6251eb487a5904267f4372281bc0c3cc6a6
RIM EL FILALI <rimo1902@gmail.com>
2022-06-03 09:12:19 -0700

    Answer to task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,32 +8,26 @@
 
 int main(void)
 {
-	int n, m, x, z;
+	int n, m;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 100 ; n++)
 	{
-		for (m = 0; m <= 9; m++)
+		for (m = n + 1; m < 100 ; m++)
 		{
-			for (x = n + 1; x <= '9'; x++)
-			{
-				for (z = m + 1; z <= '9'; z++)
-				{
-					putchar(n + '0');
-					putchar(m + '0');
-					putchar(x + '0');
-					putchar(z + '0');
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+			putchar(' ');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
 
-					if (!((n == 9 && m == 8) && (x == 9 && z == 9)))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				z = '0';
+			if (!((n == 98) && (m == 99)))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
 
-        return (0);
+	return (0);
 }
