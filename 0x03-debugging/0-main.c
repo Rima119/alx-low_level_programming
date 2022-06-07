45445921ac4694548086fe6474e4e0bdebbcf360
RIM EL FILALI <rimo1902@gmail.com>
2022-06-07 16:53:05 -0700

    Answer to task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test that the function positive_or_negative() 
+ * gives the correct output when given a case of 0.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+
+	return (0);
+}
