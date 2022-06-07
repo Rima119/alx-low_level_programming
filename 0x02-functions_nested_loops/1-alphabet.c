91dc175ebd044f9fb6a7f602f89c62e395cf8576
RIM EL FILALI <rimo1902@gmail.com>
2022-06-07 03:29:59 -0700

    Answer to task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: Write a function that prints the alphabet, in lowercase, followed by a new line.
- * Return: 0
+ * print_alphabet - function that prints the alphabet in lowercase.
+ * return: 0
  */
 
 void print_alphabet(void)
