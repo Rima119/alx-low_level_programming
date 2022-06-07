decba848af57580d14b9b4a635a5fa42bc59f0a9
RIM EL FILALI <rimo1902@gmail.com>
2022-06-07 16:31:34 -0700

    Answer to task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,16 +34,13 @@ void print_times_table(int n)
 				}
 				else if (y >= 10 && y < 100)
 				{
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((y / 10) + '0');
+					_putchar((y % 10) + '0');
 				}
 				else if (y <= 9)
 				{
-					_putchar(' ');
 					_putchar(y + '0');
 				}
-				_putchar((y % 10) + '0');
 			}
 			_putchar('\n');
 		}
