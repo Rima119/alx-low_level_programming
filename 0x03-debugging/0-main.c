c533cb16e10fec17fd6ddd342563f864b108924e
RIM EL FILALI <rimo1902@gmail.com>
2022-06-07 17:01:46 -0700

    Answer to task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - test that the function positive_or_negative() 
+ * main - test that the function positive_or_negative
  * gives the correct output when given a case of 0.
  * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	positive_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
