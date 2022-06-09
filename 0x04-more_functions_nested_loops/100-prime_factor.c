7a7f32c076332e71423443d275d59d4a1b4a2cef
RIM EL FILALI <rimo1902@gmail.com>
2022-06-09 06:23:20 -0700

    Answer to task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: finds and prints the largest prime factor of number 61285247514
+ * Return: 0
+ */
+
+int main(void)
+{
+	long x = 612852475143;
+	int m;
+
+	for (m = 2; m <= x; m++)
+	{
+		if (x % n == 0)
+		{
+			x /= m;
+			m--;
+		}
+	}
+	printf("%lu\n", x);
+}
