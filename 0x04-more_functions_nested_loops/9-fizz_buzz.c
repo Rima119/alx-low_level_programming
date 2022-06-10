a3800d2f11af3439f997936775890f43c64a5773
RIM EL FILALI <rimo1902@gmail.com>
2022-06-09 18:00:25 -0700

    Answer to task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,30 +10,26 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	a = 1;
+	printf("%d", n);
+
+	for (a = 2; a <= 100; a++)
 	{
 		if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			if (a < 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf(" Buzz");
 		}
-		else if (a % 3 == 0 && a % 5 == 0)
+		else if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", a);
+			printf(" %d", a);
 		}
 	}
 	printf("\n");
