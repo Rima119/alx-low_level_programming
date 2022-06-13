5537625f867b373e06b6def1ea9483e79458b26a
RIM EL FILALI <rimo1902@gmail.com>
2022-06-13 05:38:22 -0700

    solution to task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: char type pointer
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (s = 0; *s != '\0'; s++)
+	{
+		n++;
+	}
+
+	return (n);
+}
