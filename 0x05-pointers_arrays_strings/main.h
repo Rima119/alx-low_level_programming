8d70392bf73e0d487ba7d8783edf6830e8f9b5b3
RIM EL FILALI <rimo1902@gmail.com>
2022-06-13 05:15:15 -0700

    main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+#endif /* MAIN_H */
