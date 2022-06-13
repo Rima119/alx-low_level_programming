f00670ef3c44d8fdfc48fc01e57381b42684f48e
RIM EL FILALI <rimo1902@gmail.com>
2022-06-13 12:06:18 -0700

    solution to task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi -  function that convert a string to an integer
+ * @s: the string
+ * Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	int p = 1;
+	unsigned int m = 0;
+
+	while (*s != '\0' && !(*s <= '9' && *s >= '0'))
+	{
+		if (*s == '-')
+		{
+			p = -1 * p;
+		}
+	}
+	while (*s <= '9' && (*s >= '0' && *s != '\0'))
+	{
+		m = (m * 10) + (*s - '0');
+		*s++;
+	}
+	
+	return (m * p);
+}
