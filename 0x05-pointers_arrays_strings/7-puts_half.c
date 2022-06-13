f2d5faef667908a86af26c474c832aa0a4e81091
RIM EL FILALI <rimo1902@gmail.com>
2022-06-13 09:44:14 -0700

    solution to task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: the printed string
+ */
+
+void puts_half(char *str)
+{
+	int n, m;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+	}
+
+	m = n / 2;
+	for (++m; str[m] != '\0'; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar('\n');
+}
