5b62dd8ac9cf1bde3c998231ecc55af53c97e3d3
RIM EL FILALI <rimo1902@gmail.com>
2022-06-13 10:24:51 -0700

    solution to task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,13 @@
  * including the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: char type string
  * @src: char type string
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	char *n = dest;
-	for (; *src != '\0'; src++)
+	for (*src != '\0')
 	{
 		*dest++ = *src++;
 	}
