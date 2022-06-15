d21bc0a940a7b644dba7f42e30a22f2821ce9382
RIM EL FILALI <rimo1902@gmail.com>
2022-06-15 15:25:52 -0700

    solution to task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @p: string
+ * Return: encoded string
+ */
+
+char *rot13(char *p)
+{
+	int n;
+	int m;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (n = 0; p[n] != '\0'; n++)
+	{
+		for (m = 0; l[m] != '\0'; m++)
+		{
+			if (p[n] == l[m])
+			{
+				p[n] == u[m];
+			}
+		}
+	}
+	return (p);
+}
