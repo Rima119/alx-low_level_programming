bdbcff5eb2662a84b86f272747815ae5790b6d3b
RIM EL FILALI <rimo1902@gmail.com>
2022-06-15 14:42:58 -0700

    solution to task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @p: string
+ * Return: encoded string
+ */
+
+char *leet(char *p)
+{
+	int n = 0;
+
+	while (p[n] != '\0')
+	{
+		if (p[n] == 'a' || p[n] == 'A')
+		{
+			p[n] == '4';
+		}
+		if (p[n] == 'e' || p[n] == 'E')
+		{
+			p[n] == '3';
+		}
+		if (p[n] == 'o' || p[n] == 'O')
+		{
+			p[n] == '0';
+		}
+		if (p[n] == 't' || p[n] == 'T')
+		{
+			p[n] == '7';
+		}
+		if (p[n] == 'l' || p[n] == 'L')
+		{
+			p[n] == '1';
+		}
+	}
+	return (p);
+}
