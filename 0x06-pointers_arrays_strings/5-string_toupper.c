f8451e4a1134f2710e5c750be3d1b3c5012b3138
RIM EL FILALI <rimo1902@gmail.com>
2022-06-15 10:52:37 -0700

    solution to task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,6 +3,8 @@
 /**
  * string_toupper - function that changes all lowercase
  * letters of a string to uppercase
+ * @p: string
+ * Return: uppercase letters of a string
  */
 
 char *string_toupper(char *p)
