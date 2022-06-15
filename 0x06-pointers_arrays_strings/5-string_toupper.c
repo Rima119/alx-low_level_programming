c7b011262c1a4ffadae3f055e4fd73a03e6715ba
RIM EL FILALI <rimo1902@gmail.com>
2022-06-15 10:47:13 -0700

    solution to task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase
+ * letters of a string to uppercase
+ */
+
+char *string_toupper(char *p)
+{
+	while (*p != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s -= 32;
+		}
+		s++;
+	}
+	return (s);
+}
