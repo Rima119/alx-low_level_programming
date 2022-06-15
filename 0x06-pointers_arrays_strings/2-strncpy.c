15b6e27e7df61166ca93abf025eddc780e08c3fd
RIM EL FILALI <rimo1902@gmail.com>
2022-06-15 10:03:49 -0700

    solution to task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,7 @@
  * Return: a pointer to the resulting string dest
  */
 
-char *_strncat(char *dest, char *src, int n)
-{
+char *_strncpy(char *dest, char *src, int n){
 	int p = 0;
 
 	while (n > p)
