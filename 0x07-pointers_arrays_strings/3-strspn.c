de03875cd7f2a39488ede5cac41240f94fa2b64f
RIM EL FILALI <rimo1902@gmail.com>
2022-06-20 11:36:12 -0700

    solution to task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: the initial segment
+ * @accept: the substring
+ * Return: number of bytes in initial segment s consisting of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n, m;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		for (m = 0; accept[m] != '\0'; m++)
+		{
+			if (s[n] == accept[m])
+			{
+				break;
+			}
+		}
+		if (!accept[m])
+		{
+			break;
+		}
+
+	}
+	return (n);
+}
