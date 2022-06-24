e392a02b38e72303d34fbfaf3958c812e59d841b
RIM EL FILALI <rimo1902@gmail.com>
2022-06-24 10:35:01 -0700

    solution to task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguement
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+	int m = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+
+	while (n > 0)
+	{
+		if (n >= 25)
+		{
+			n = n - 25;
+		}
+		if (n >= 10)
+		{
+			n = n - 10;
+		}
+		if (n >= 5)
+		{
+			n = n - 5;
+		}
+		if (n >= 2)
+		{
+			n = n - 2;
+		}
+		if (n >= 1)
+		{
+			n = n - 1;
+		}
+		m++
+	}
+	printf("%d\n", m);
+	return (0);
+}
