443ffebf51a38853215f00ee2de617d1cdc80302
RIM EL FILALI <rimo1902@gmail.com>
2022-06-24 09:56:54 -0700

    solution to task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguement
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
