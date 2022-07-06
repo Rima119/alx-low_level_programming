fcb2333a71955fbaa3ca821d29c18952a56f63d4
RIM EL FILALI <rimo1902@gmail.com>
2022-07-06 15:01:25 -0700

    solution to task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * op_add - function for addition
@@ -47,7 +49,8 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		return (-1);
+		printf("Error\n");
+		exit(100);
 	}
 	return (a / b);
 }
@@ -63,7 +66,8 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		return (-1);
+		printf("Error\n");
+		exit(100);
 	}
 	return (a % b);
 }
