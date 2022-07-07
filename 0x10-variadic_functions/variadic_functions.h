68f753841c3a2ea776dd9f14f2fd9f68e3367d13
RIM EL FILALI <rimo1902@gmail.com>
2022-07-07 10:24:33 -0700

    variadic_functions file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,12 +7,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
+
+/**
+ * struct pristr - struct op
+ * @m: operator
+ * @pr: the function
+ */
 
 typedef struct pristr
 {
 	char *m;
 	void (*pr)(va_list v);
 } pristr_t;
+
 void pr_char(va_list v);
 void pr_integer(va_list v);
 void pr_float(va_list v);
