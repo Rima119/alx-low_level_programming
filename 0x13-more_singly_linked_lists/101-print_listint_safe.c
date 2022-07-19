08f643947f557a2dd92c71fbdc4bbba3ca272e58
RIM EL FILALI <rimo1902@gmail.com>
2022-07-19 16:12:54 -0700

    solution tp task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,7 @@
 /**
  * print_listint_safe - function that prints a listint_t linked list
  * @head: head of the linked list
- * Return: the number of nodes in the list
+ * Return: the number of nodes in the linked list
  */
 
 size_t print_listint_safe(const listint_t *head)
