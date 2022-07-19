cbbb1610668f74e1506bfb6aa4cb347e2ad0f793
RIM EL FILALI <rimo1902@gmail.com>
2022-07-19 11:00:45 -0700

    solution to task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a listint_t list.
+ * @h: head of the linked list
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *m, *b;
+	size_t a = 0;
+
+	m = *h;
+	while (m != NULL)
+	{
+		a += 1;
+		b = m;
+		m = m->next;
+		free(b);
+		if (b < m)
+		{
+			break;
+		}
+	}
+	*h = NULL;
+	return (a);
+}
