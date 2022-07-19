6a6b4a808805f723d70d60dc9aa8f28cecf4af14
RIM EL FILALI <rimo1902@gmail.com>
2022-07-19 07:45:28 -0700

    solution to task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,19 +9,21 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *m, *b;
-	size_t p = 0;
+	size_t p;
 
 	m = head;
 	if (m == NULL)
 	{
 		exit(98);
 	}
+	p = 0;
 	while (m != NULL)
 	{
 		b = m;
 		m = m->next;
 		p++;
 		printf("[%p] %d\n", (void *)b, b->n);
+		
 		if (b < m)
 		{
 			printf("-> [%p] %d\n", (void *)m, m->n);
