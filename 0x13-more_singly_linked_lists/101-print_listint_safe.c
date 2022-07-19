c7ec872c35156ead02da0c644711db360488cff5
RIM EL FILALI <rimo1902@gmail.com>
2022-07-19 07:53:54 -0700

    solution to task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -23,7 +23,6 @@ size_t print_listint_safe(const listint_t *head)
 		m = m->next;
 		p++;
 		printf("[%p] %d\n", (void *)b, b->n);
-		
 		if (b < m)
 		{
 			printf("-> [%p] %d\n", (void *)m, m->n);
