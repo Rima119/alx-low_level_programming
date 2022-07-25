6b6b0e8aae52af46bd679a7c3ce9166d95a75091
RIM EL FILALI <rimo1902@gmail.com>
2022-07-25 15:09:03 -0700

    solution to task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: the file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int n;
+	char *m;
+	ssize_t rct, wct;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	m = malloc(sizeof(char) * letters);
+	if (m == NULL)
+	{
+		return (0);
+	}
+	rct = read(n, m, letters);
+	wct = write(STDOUT_FILENO, m, rct);
+
+	close(n);
+	free(m);
+	return (wct);
+}
