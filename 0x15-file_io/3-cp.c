6750833a636737fc945269d455a590b4704e6b79
RIM EL FILALI <rimo1902@gmail.com>
2022-07-25 16:19:21 -0700

    solution to task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, m, p, d;
+	char s[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	n = open(argv[1], O_RDONLY);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	m = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (m == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while (p != 0)
+	{
+		d = write(m, s, p);
+		if (d == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			exit(99);
+		}
+		p = read(n, s, 1024);
+		if (p == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+	}
+	p = close(n);
+	if (p == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", n);
+		exit(100);
+	}
+	d = close(m);
+	if (d == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", m);
+		exit(100);
+	}
+	return (0);
+}
