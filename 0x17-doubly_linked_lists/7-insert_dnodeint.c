6e48d5f643f54307e6e1fc88b398fcaa6254ba06
RIM EL FILALI <rimo1902@gmail.com>
2022-09-01 12:09:08 -0700

    solution to task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *wnode, *m;
+	dlistint_t *tmp, *wnode;
 	unsigned int p;
 
 	wnode = malloc(sizeof(dlistint_t));
