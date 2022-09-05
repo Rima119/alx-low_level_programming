7ca045a6c30f279e190fd79080df23d35afcceb0
RIM EL FILALI <rimo1902@gmail.com>
2022-09-05 03:20:00 -0700

    solution to task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
-Debugger entered--Lisp error: (void-variable gcc)
-  eval-region(1 16 t #f(compiled-function (ignore) #<bytecode 0x46f3ed>))  ; Reading at buffer position 58
-  elisp--eval-defun()
-  eval-defun(nil)
-  funcall-interactively(eval-defun nil)
-  call-interactively(eval-defun nil nil)
-  command-execute(eval-defun)
+#!/bin/bash
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
