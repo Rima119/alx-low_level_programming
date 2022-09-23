ed4fca9736a1505cdb963c784be4ba54debdf9f5
RIM EL FILALI <rimo1902@gmail.com>
2022-09-23 09:25:55 -0700

    solution to task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int n;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		for (n = 0; n < ht->size; n++)
+		{
+			temp = ht->array[n];
+			while (temp != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+			}
+		}
+		printf("}\n");
+	}
+}
