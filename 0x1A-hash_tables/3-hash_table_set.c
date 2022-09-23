9d749ecc6e0a09ef05e68331bc44b209fbd5c3f3
RIM EL FILALI <rimo1902@gmail.com>
2022-09-23 09:04:08 -0700

    solution to task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	temp = malloc(sizeof(hash_node_t));
-	if (tmp == NULL)
+	if (temp == NULL)
 		return (0);
 	temp->key = strdup(key);
 	temp->value = strdup(value);
