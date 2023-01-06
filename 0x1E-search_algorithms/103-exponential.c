cf87c8764627b5fa208f6f9b5f1cf1edaf4807bc
Rim El Filali <rimo1902@gmail.com>
2023-01-06 12:37:49 -0800

    add

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+
+/**
+ * bn_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * array: pointer to the first element of the array to search in
+ * @low: start index of the array to search in
+ * @high: end index of the array to search in
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+int bn_search(int *array, size_t low, size_t high, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		
+		i = low + (high - low) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] < value)
+			low = i + 1;
+		else
+			high = i - 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers
+ * using the Exponential search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0, low, high;
+	
+	if (array == NULL)
+		return (-1);
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	low = i / 2;
+	high = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", low, high);
+	return (bn_search(array, low, high, value));
+}
